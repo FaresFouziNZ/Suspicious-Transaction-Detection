a794d19e8d142ab27e6453c27e7fda63986bc8b9
FaresFouziNZ <74138397+FaresFouziNZ@users.noreply.github.com>
2025-08-27 21:39:58 +0300

    Integrate HermesService for transactions

--- etl-pipeline/src/main/scala/etl/Main.scala
+++ etl-pipeline/src/main/scala/etl/Main.scala
@@ -5,13 +5,17 @@ import zio.logging.backend.SLF4J
 
 object Main extends ZIOAppDefault {
 
-  val program: ZIO[AppConfig & MinioService, Throwable, Unit] = for {
+  val program: ZIO[AppConfig & MinioService & HermesService, Throwable, Unit] = for {
     cfg <- ZIO.service[AppConfig]
     minio <- ZIO.service[MinioService]
     _     <- ZIO.logInfo(s"Minio bucket: ${cfg.minio.bucket} started ✅")
     txns  <- minio.readAllTransactions(cfg.minio.bucket)
     _     <- ZIO.logInfo(s"Found ${txns.size} transactions in bucket ${cfg.minio.bucket}")
+    hermes <- ZIO.service[HermesService]
+    enrichedTxns <- hermes.enrichBatch(txns)
+    _ <- ZIO.foreach(enrichedTxns)(f => ZIO.logInfo(s"${f}"))
+
   } yield ()
 
-  override def run = program.provide(ConfigLoader.layer, MinioService.live)
+  override def run = program.provide(ConfigLoader.layer, MinioService.live, HermesService.live)
 }
